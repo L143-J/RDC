c1931feb18ed470d4baa7920573a3c8b4a4ca189
Barbosa <laismorton@gmail.com>
2026-09-12 17:50:05 -0300

    Add files via upload

---
+++ soma.py
@@ -0,0 +1,5 @@
+print("Bem-vindo ao programa de soma!")
+x = int(input("Digite o primeiro número: "))
+y = int(input("Digite o segundo número: "))
+z = x + y
+print("A soma dos dois números é: ", z)
